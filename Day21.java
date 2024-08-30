14bb59691a438123813df2610c8d4261804e0699
sriswasthika <155260452+sriswasthika@users.noreply.github.com>
2024-08-30 23:34:53 +0530

    Create Day21.java

---
+++ Day21.java
@@ -0,0 +1,16 @@
+Pattern
+
+public class AlphabetPattern {
+    public static void main(String[] args) {
+        int rows = 5; // You can change this to print more or less rows
+
+        // Print the alphabet pattern
+        for (int i = 0; i < rows; i++) {
+            char ch = (char) ('A' + i);
+            for (int j = 0; j <= i; j++) {
+                System.out.print(ch + " ");
+            }
+            System.out.println();
+        }
+    }
+}
