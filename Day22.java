ba321a1ba5ea169f0c79143ff70554d4408aa82a
sriswasthika <155260452+sriswasthika@users.noreply.github.com>
2024-08-31 23:09:07 +0530

    Create Day22.java

---
+++ Day22.java
@@ -0,0 +1,15 @@
+Pattern
+  
+public class IncrementNumbersPattern {
+    public static void main(String[] args) {
+        int rows = 4; 
+        int num = 1;
+        for (int i = 1; i <= rows; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(num + " ");
+                num++;
+            }
+            System.out.println();
+        }
+    }
+}
