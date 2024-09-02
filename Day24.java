2b40a668ddba7252f18dc26244b1ce23c96008fe
sriswasthika <155260452+sriswasthika@users.noreply.github.com>
2024-09-03 00:25:36 +0530

    Create Day24.java

---
+++ Day24.java
@@ -0,0 +1,16 @@
+
+import java.util.Scanner;
+
+public class Palindrome {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a string: ");
+        String input = scanner.nextLine();
+        scanner.close();
+
+        if (isPalindrome(input)) {
+            System.out.println(input + " is a palindrome.");
+        } else {
+            System.out.println(input + " is not a palindrome.");
+        }
+```
