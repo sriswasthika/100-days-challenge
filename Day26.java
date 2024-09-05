6fefaf1853e5c1957078493fbf8fde4d2ca75253
sriswasthika <155260452+sriswasthika@users.noreply.github.com>
2024-09-05 22:01:04 +0530

    Create Day26.java

---
+++ Day26.java
@@ -0,0 +1,13 @@
+Increment operators
+
+public class IncrementOperators {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 5;
+        System.out.println("Pre-increment: " + ++a);
+        System.out.println("Post-increment: " + b++);
+
+        System.out.println("Value of a: " + a);
+        System.out.println("Value of b: " + b);
+    }
+}
